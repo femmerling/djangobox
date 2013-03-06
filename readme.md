9c85dbe348a094bb1065d05ec515a17873a9e377
Fauzan Erich Emmerling <erich@emfeld.com>
2013-03-06 17:36:29 +0700

    Initial package

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Description
+
+You will need to setup several python projects and you will have different libraries for all those projects. Use this localized library to work with and help you have a better day with Django.
+
+to use, simple run:
+
+	python setup.py
+
+Voila! There you go, your django environment is ready!
---
+++ setup.py
@@ -0,0 +1,19 @@
+from subprocess import call
+
+print "\nCreating Virtual Environment\n"
+print "============================\n"
+call(['python','virtualenv.py','box'])
+print "\nVirtual Environment Created\n"
+print "\nInstalling Django\n"
+print "=================\n"
+call(['box/bin/pip','install','django'])
+print "\nDjango Installed\n"
+print "\nInstalling PostgreSQL Driver\n"
+print "============================\n"
+call(['box/bin/pip','install','psycopg2'])
+print "\nPostgreSQLDriver Installed\n"
+print "\nInstalling MySQL Driver\n"
+print "=======================\n"
+call(['box/bin/pip','install','mysql-python'])
+print "\nMySQLDriver Installed\n"
+print "\n\nYour new Django Environment is ready!\n\n"
